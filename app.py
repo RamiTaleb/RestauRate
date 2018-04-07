bb60a4f561eb5b09120c25fd0204882ca8d96e20
Rami Taleb <rtale089@uottawa.ca>
2018-04-06 23:08:32 -0400

    added favicon, logo, changed query page to take model, added query pages

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from dbFiller import populateRestaurantTable, populateMenuTable, populateRaterTable, populateRatingTable, populateMenuItemRatingTable
+from dbFiller import *
 from settings import app, db
+from models import *
+from flask import render_template, redirect, url_for
+from sqlalchemy import text
+
 
 #populateRaterTable(db)
 
@@ -15,7 +19,15 @@ from settings import app, db
 
 @app.route('/')
 def index():
-	return "<h1 style='color: red'>Hello World<h1>"
+	return render_template('index.html')
+
+
+@app.route('/query')
+def query():
+	sql = text('select * from menu_item')
+	raters = db.engine.execute(sql)
+	print ('SELECT * FROM Restaurant R, Location L WHERE R.name = $restaurantName AND R.\"restaurantId\" = L.restaurant;')
+	return render_template('query.html', raters=raters)
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.debug = True
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/Project'
 
